e1c8e9a940a668383aff03576ff4a1fb5f391a57
Vitaly Stukov <vstukov@beltechsoftware.com>
2020-02-08 23:57:16 +0300

    Improve finder.

--- app/javascript/components/ArticleFinder.js
+++ app/javascript/components/ArticleFinder.js
@@ -4,11 +4,11 @@ import { observer } from "mobx-react";
 
 @observer
 class ArticleFinder extends React.Component {
-  handleChangeValue = (e) => {
+  onChangeSearchValue = (e) => {
     this.props.tableStore.search_value = e.target.value;
   };
 
-  handleChangeSearchField = (e) => {
+  onChangeSearchField = (e) => {
     this.props.tableStore.search_field = e.target.value;
   };
 
@@ -16,37 +16,36 @@ class ArticleFinder extends React.Component {
     return `Search by ${this.props.tableStore.search_field}...`;
   }
 
+  onSearch = () => {
+    this.props.store.searchArticles();
+  };
+
   render() {
     return (
       <div className="input-group mb-3">
         <div className="input-group-prepend">
           <div className="input-group-text">
             <div className="custom-control custom-radio custom-control-inline">
-              <input onChange={this.handleChangeSearchField} value="name" defaultChecked type="radio" id="customRadioInline1" name="customRadioInline1" className="custom-control-input">
+              <input onChange={this.onChangeSearchField} value="name" defaultChecked type="radio" id="customRadioInline1" name="customRadioInline1" className="custom-control-input">
               </input>
               <label className="custom-control-label" htmlFor="customRadioInline1">Name</label>
             </div>
             <div className="custom-control custom-radio custom-control-inline">
-              <input onChange={this.handleChangeSearchField} value="text" type="radio" id="customRadioInline2" name="customRadioInline1" className="custom-control-input">
+              <input onChange={this.onChangeSearchField} value="text" type="radio" id="customRadioInline2" name="customRadioInline1" className="custom-control-input">
               </input>
               <label className="custom-control-label" htmlFor="customRadioInline2">Text</label>
             </div>
           </div>
         </div>
 
-        <input onChange={this.handleChangeValue} type="text" className="form-control" placeholder={this.getPlaceholder()}
-               aria-label="Recipient's username">
+        <input onChange={this.onChangeSearchValue} type="text" className="form-control" placeholder={this.getPlaceholder()}>
         </input>
         <div className="input-group-append">
           <button onClick={this.onSearch} className="btn btn-outline-secondary" type="button">Search</button>
         </div>
       </div>
     );
-  };
-
-  onSearch = () => {
-     this.props.store.searchArticles();
-  };
-};
+  }
+}
 
 export default ArticleFinder;
--- app/javascript/components/ObservableTableStore.js
+++ app/javascript/components/ObservableTableStore.js
@@ -1,17 +1,14 @@
-import React, {Component} from "react";
-import ReactDOM, { render } from "react-dom";
-import MobX, { observable, action, computed, autorun } from "mobx";
+import { observable } from "mobx";
 
 class ObservableTableStore {
   @observable search_field;
-  @observable search_value;
-  @observable sort_field;
-  @observable sort_direction;
-  @observable group_field;
 
   constructor() {
     this.search_field = 'name';
-    this.stories = {};
+    this.search_value = null;
+    this.sort_field = null;
+    this.sort_direction = null;
+    this.group_field = null;
   }
 }
 
